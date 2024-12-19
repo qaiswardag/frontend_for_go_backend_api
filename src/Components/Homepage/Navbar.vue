08d3ad1ea49f364db17b3810f87f03b9235595ae
qaiswardag <qais.wardag@outlook.com>
2024-12-19 18:51:13 +0100

    Remove sign out button from Navbar component

--- src/Components/Homepage/Navbar.vue
+++ src/Components/Homepage/Navbar.vue
@@ -35,14 +35,6 @@ const handleLogOut = async function () {
           >
             Sign up
           </RouterLink>
-
-          <button
-            class="text-myPrimaryDarkGrayColor group flex items-center px-4 py-2 font-normal text-base gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50 justify-center"
-            type="button"
-            @click="handleLogOut"
-          >
-            Sign out
-          </button>
           <span
             class="text-myPrimaryDarkGrayColor group flex items-center px-4 py-2 font-normal text-base gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50 justify-center"
           >
