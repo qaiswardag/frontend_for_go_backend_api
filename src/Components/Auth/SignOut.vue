5405cbde146e768a03566343346a3e25cb896c23
qaiswardag <qais.wardag@outlook.com>
2025-03-17 13:28:59 +0100

    Remove redundant padding class from SignOut button for cleaner styling

--- src/Components/Auth/SignOut.vue
+++ src/Components/Auth/SignOut.vue
@@ -21,7 +21,7 @@ const getUser = computed(() => {
 </script>
 <template>
   <button
-    class="text-myPrimaryDarkGrayColor group flex items-center font-normal text-base gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50 relative"
+    class="text-myPrimaryDarkGrayColor group flex items-center font-normal gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50 relative"
     :class="{
       'py-2 px-6': layoutDesign === 'topMainMenu',
       'py-4 pl-6 pr-2 w-full': layoutDesign === 'leftSidebar',
