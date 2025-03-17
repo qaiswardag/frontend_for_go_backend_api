76979960bc48c2d82f9c010302d09a2a01e08938
qaiswardag <qais.wardag@outlook.com>
2025-03-17 13:29:11 +0100

    Fix NavbarLeft layout by adding float-left class for improved alignment

--- src/Components/Homepage/NavbarLeft.vue
+++ src/Components/Homepage/NavbarLeft.vue
@@ -16,7 +16,7 @@ import SignOut from '@/Components/Auth/SignOut.vue';
       </p>
     </div>
     <nav aria-label="Global">
-      <div class="flex-1 space-y-4 px-2 pb-4">
+      <div class="flex-1 space-y-4 px-2 pb-4 float-left">
         <RouterLink
           class="text-myPrimaryDarkGrayColor group flex items-center pl-6 pr-2 py-4 font-normal text-base gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50"
           :to="{ name: 'Account' }"
