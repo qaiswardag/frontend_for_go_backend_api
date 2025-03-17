1f76e614ed61565e3ddcfd2fa2c5bbeb86229ff1
qaiswardag <qais.wardag@outlook.com>
2025-03-17 13:29:18 +0100

    Add Home page component with data fetching functionality

---
+++ src/Pages/Home.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { vueFetch } from '@/composables/vueFetch';
+import { ref, onMounted } from 'vue';
+
+const {
+  handleData,
+  fetchedData,
+  isError,
+  error,
+  errors,
+  isLoading,
+  isSuccess,
+} = vueFetch();
+
+const fetchData = async () => {
+  try {
+    await handleData(`${import.meta.env.VITE_BACKEND_URL}/`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (err) {
+    console.error('Error fetching data:', err);
+  }
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+
+<template>
+  <div>
+    <h2 class="myTertiaryHeader text-center py-12 bg-slate-600 text-white">
+      Home
+    </h2>
+    <p>Home content here...</p>
+
+    <p>fetchedData: {{ fetchedData }}</p>
+  </div>
+</template>
