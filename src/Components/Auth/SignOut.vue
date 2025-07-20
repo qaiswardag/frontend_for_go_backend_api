6ba2389cdeac312e8a145d9776ca712457d36ab0
Qais Wardag <qais.wardag@outlook.com>
2025-07-20 12:08:22 +0200

    Refactor SignOut button structure for improved readability and maintainability

--- src/Components/Auth/SignOut.vue
+++ src/Components/Auth/SignOut.vue
@@ -21,39 +21,40 @@ const getUser = computed(() => {
 </script>
 <template>
   <div>
-    <template v-if="getUser">
-      <button
-        class="text-myPrimaryDarkGrayColor group flex items-center font-normal gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50 relative"
+    <button
+      class="text-myPrimaryDarkGrayColor group flex items-center font-normal gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50 relative"
+      :class="{
+        'py-2 px-6': layoutDesign === 'topMainMenu',
+        'py-4 pl-4 pr-2 w-full': layoutDesign === 'leftSidebar',
+      }"
+      type="button"
+      @click="handleLogOut"
+    >
+      <template v-if="layoutDesign === 'leftSidebar'">
+        <span class="material-symbols-outlined"> arrow_right_alt </span>
+      </template>
+
+      <!-- Visible text that changes dynamically -->
+      <span
+        class="absolute inset-0 flex items-center justify-center"
         :class="{
-          'py-2 px-6': layoutDesign === 'topMainMenu',
-          'py-4 pl-4 pr-2 w-full': layoutDesign === 'leftSidebar',
+          '': layoutDesign === 'topMainMenu',
+          '': layoutDesign === 'leftSidebar',
         }"
-        type="button"
-        @click="handleLogOut"
       >
-        <template v-if="layoutDesign === 'leftSidebar'">
-          <span class="material-symbols-outlined"> arrow_right_alt </span>
+        <template v-if="getUser && !getUser.isLoadingSignOut">
+          <span>Sign out</span>
         </template>
+        <template v-if="!getUser">
+          <span>Sign out</span>
+        </template>
+        <template v-if="getUser && getUser.isLoadingSignOut">
+          <span>Signing out..</span>
+        </template>
+      </span>
 
-        <!-- Visible text that changes dynamically -->
-        <span
-          class="absolute inset-0 flex items-center justify-center"
-          :class="{
-            '': layoutDesign === 'topMainMenu',
-            '': layoutDesign === 'leftSidebar',
-          }"
-        >
-          <template v-if="getUser && !getUser.isLoadingSignOut">
-            <span>Sign out</span>
-          </template>
-          <template v-if="getUser && getUser.isLoadingSignOut">
-            <span>Signing out..</span>
-          </template>
-        </span>
-
-        <!-- Invisible text for consistent button width -->
-        <span class="invisible">Signing out...</span>
-      </button>
-    </template>
+      <!-- Invisible text for consistent button width -->
+      <span class="invisible">Signing out...</span>
+    </button>
   </div>
 </template>
