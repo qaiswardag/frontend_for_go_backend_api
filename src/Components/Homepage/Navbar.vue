2c6040fad99fe53b8fe7ab2ecea9f84644c5c087
Qais Wardag <qais.wardag@outlook.com>
2025-07-21 10:30:13 +0200

    Refactor Navbar component to enhance user data display logic and improve code structure

--- src/Components/Homepage/Navbar.vue
+++ src/Components/Homepage/Navbar.vue
@@ -2,6 +2,19 @@
 import { RouterLink } from 'vue-router';
 import UserBadge from '@/Components/Auth/UserBadge.vue';
 import SignOut from '@/Components/Auth/SignOut.vue';
+
+import { computed, ref } from 'vue';
+import { useUserStore } from '@/stores/user';
+
+const userStore = useUserStore();
+
+const getIsLoading = computed(() => {
+  return userStore.getIsLoading;
+});
+
+const getUser = computed(() => {
+  return userStore.getUser;
+});
 </script>
 
 <template>
@@ -19,7 +32,19 @@ import SignOut from '@/Components/Auth/SignOut.vue';
             Home
           </RouterLink>
         </div>
+
         <div class="flex items-center justify-end myPrimaryGap pr-4">
+          <div class="flex items-center justify-end myPrimaryGap pr-4">
+            {{
+              getUser &&
+              getUser.fetchedDataSignIn &&
+              getUser.fetchedDataSignIn &&
+              getUser.fetchedDataSignIn.user &&
+              getUser.fetchedDataSignIn.user.FirstName +
+                ' ' +
+                getUser.fetchedDataSignIn.user.LastName
+            }}
+          </div>
           <RouterLink
             class="text-myPrimaryDarkGrayColor group flex items-center py-2 px-6 gap-3 rounded-full hover:bg-myPrimaryLightGrayColor bg-gray-50 justify-center"
             :to="{ name: 'SignIn' }"
